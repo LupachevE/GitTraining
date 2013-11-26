b3c222b63ccaf0765fac1f254357d6cc681411d2
Lupachev Evgeniy <jon-apa4@yandex.ru>
2013-11-26 19:11:43 +0400

    Added unit tests and GUI

---
+++ CheckUrE-mail/Program.fs
@@ -0,0 +1,58 @@
+﻿open System
+open NUnit.Framework
+open FsUnit
+open System.Text.RegularExpressions
+
+let thirdlevel = "^[a-zA-Z_][a-zA-Z0-9]*[\.]?[_-]*[a-zA-Z0-9]*"
+let secondlevel = "[@][a-zA-Z]{1,30}"
+let firstlevel = "\.(aero|arpa|asia|coopinfo|domain|info|jobs|mobi|museum|name|travel|yandex|[a-z]{2,3})$"
+let pattern = thirdlevel + secondlevel + firstlevel
+let checkEmail string = Regex.IsMatch(string, pattern)
+
+(*Old tests
+checkEmail "a@b.cc"
+checkEmail "victor.polozov@mail.ru"
+checkEmail "my@domain.info"
+checkEmail "_.1@mail.com"
+checkEmail "coins_department@hermitage.museum"
+
+checkEmail "a@b.c"
+checkEmail "a..b@mail.ru"
+checkEmail ".a@mail.ru"
+checkEmail "yo@domain.somedomain"
+checkEmail "1@mail.ru"
+*)
+
+//New tests
+[<TestFixture>]
+type Tests() = 
+  
+  [<Test>]
+  member this.``All 3 levels are short`` () = checkEmail "a@b.c" |> should be True
+  
+  [<Test>]
+  member this.``Standart e-mail`` () = checkEmail "victor.polozov@mail.ru" |> should be True
+  
+  [<Test>]
+  member this.``1st level domain - info`` () = checkEmail "my@domain.info" |> should be True
+  
+  [<Test>]
+  member this.``Starts with '_'`` () = checkEmail "_.1@mail.com" |> should be True
+  
+  [<Test>]
+  member this.``Strange and long, but correct`` () = checkEmail "coins_department@hermitage.museum" |> should be True
+  
+  [<Test>]
+  member this.``Too short 1st level domain`` () = checkEmail "a@b.c" |> should be False
+  
+  [<Test>]
+  member this.``Double dot`` () = checkEmail "a..b@mail.ru" |> should be False
+  
+  [<Test>]
+  member this.``Wrong 1st level domain`` () = checkEmail "yo@domain.somedomain" |> should be False
+ 
+  [<Test>]
+  member this.``Starts with dot`` () = checkEmail ".a@mail.ru" |> should be False
+ 
+  [<Test>] 
+  member this.``Only number`` () = checkEmail "1@mail.ru" |> should be False
---
+++ GUI & Events/Program.fs
@@ -0,0 +1,79 @@
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+
+let mutable balance = 0
+
+let draw(this : string) =
+  printfn "%A" 1
+
+type Cars(price : int, model : string) =
+  
+  let price = price
+  let model = model
+  member this.Price = price
+  member this.Model = model
+  member this.IsEnough(balance : int) = balance >= price 
+  member this.Draw(model) =
+    match model with
+    | "Audi"       -> draw("Audi")
+    | "Toyota"     -> draw("Toyota")
+    | "Honda"      -> draw("Honda")
+    | "Volkswagen" -> draw("Volkswagen")
+    | "Ford"       -> draw("Ford")
+    | "Mitsubishi" -> draw("Mitsubishi")
+    | "Mersedes"   -> draw("Mersedes")
+    | _            -> draw("Nothinng")
+
+let OnFeet = new Cars(0, "Foot")
+let Audi = new Cars(1000, "Audi")
+let Toyota = new Cars(5000, "Toyota")
+let Honda = new Cars(15000, "Honda")
+let Volkswagen = new Cars(30000, "VolksWagen")
+let Ford = new Cars(100000, "Ford")
+let Mitsubishi = new Cars(300000, "Mitsubishi")
+let Mersedes = new Cars(1000000, "Mersedes")
+
+let CarForm = 
+    
+    let form = new Form(Text = "Choosing the car")
+    let button = new Button(Text = "Huy", Left = 10,
+                                Top = 10, Width = 80, Enabled = true)
+
+    form
+
+let LuckyForm = 
+    
+    let form = new Form(Text = "Lucky or Not")
+    let button = new Button(Text = "Press it!", Left = 400,
+                            Top = 600, Width = 80, Enabled = true)
+    form
+
+let mainForm = 
+
+    //if not (this.IsEnough(balance)) then button.Enabled <- not button.Enabled
+
+    let form = new Form(Text = "Cars&Money")
+
+    let firstbutton = new Button(Text = "ChooseYourCar", Left = 10,
+                                 Top = 10, Width = 100, Enabled = true)
+    let secondbutton = new Button(Text = "TestYourLuck", Left = 150,
+                                  Top = 10, Width = 100, Enabled = true)
+             
+                                                                                                
+    let buttonPress1 _ _ = LuckyForm.ShowDialog |> ignore
+    let buttonPress2 _ _ = CarForm.ShowDialog |> ignore
+
+    let eventHandler1 = new EventHandler(buttonPress1)
+    let eventHandler2 = new EventHandler(buttonPress2)
+
+    firstbutton.Click.AddHandler(eventHandler1)
+    secondbutton.Click.AddHandler(eventHandler2) 
+
+    let dc c = (c :> Control)
+
+    form.Controls.AddRange([| dc firstbutton; dc secondbutton;|])
+
+    form
+
+do Application.Run(mainForm)
