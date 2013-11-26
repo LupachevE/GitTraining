612c39efe930460c332d4cf485d2f04862021f06
Lupachev Evgeniy <jon-apa4@yandex.ru>
2013-11-26 20:39:18 +0400

    Added some functions in type Cars, new buttons to the forms

--- GUI & Events/Program.fs
+++ GUI & Events/Program.fs
@@ -2,45 +2,59 @@
 open System.Windows.Forms
 open System.Drawing
 
-let mutable balance = 0
-
-let draw(this : string) =
-  printfn "%A" 1
-
-type Cars(price : int, model : string) =
+type Cars(price : int, model : string, ern : int) =
   
   let price = price
   let model = model
-  member this.Price = price
-  member this.Model = model
-  member this.IsEnough(balance : int) = balance >= price 
-  member this.Draw(model) =
+//Функция draw пока с заглушками, на их месте должна быть отрисовка каждой модели по отдельности
+  let draw model =
     match model with
-    | "Audi"       -> draw("Audi")
-    | "Toyota"     -> draw("Toyota")
-    | "Honda"      -> draw("Honda")
-    | "Volkswagen" -> draw("Volkswagen")
-    | "Ford"       -> draw("Ford")
-    | "Mitsubishi" -> draw("Mitsubishi")
-    | "Mersedes"   -> draw("Mersedes")
-    | _            -> draw("Nothinng")
+    | "Audi"       -> printfn "1"
+    | "Toyota"     -> printfn "1"
+    | "Honda"      -> printfn "1"
+    | "Volkswagen" -> printfn "1"
+    | "Ford"       -> printfn "1"
+    | "Mitsubishi" -> printfn "1"
+    | "Mersedes"   -> printfn "1"
+    | _            -> printfn "1" 
+  member this.Price = price
+  member this.Ern = ern
+  member this.Model = model
+  member this.IsEnough(balance : int) = balance >= price
+  member this.Draw(model) = draw(this.Model)
+  
 
-let OnFeet = new Cars(0, "Foot")
-let Audi = new Cars(1000, "Audi")
-let Toyota = new Cars(5000, "Toyota")
-let Honda = new Cars(15000, "Honda")
-let Volkswagen = new Cars(30000, "VolksWagen")
-let Ford = new Cars(100000, "Ford")
-let Mitsubishi = new Cars(300000, "Mitsubishi")
-let Mersedes = new Cars(1000000, "Mersedes")
+let OnFeet = new Cars(0, "Foot", 10)
+let Audi = new Cars(1000, "Audi", 50)
+let Toyota = new Cars(5000, "Toyota", 100)
+let Honda = new Cars(15000, "Honda", 300)
+let Volkswagen = new Cars(30000, "VolksWagen", 1000)
+let Ford = new Cars(100000, "Ford", 2500)
+let Mitsubishi = new Cars(300000, "Mitsubishi", 10000)
+let Mersedes = new Cars(1000000, "Mersedes", 50000)
+
+let mutable balance = 0
+let mutable (CurrentCar : Cars) = OnFeet 
 
 let CarForm = 
     
-    let form = new Form(Text = "Choosing the car")
-    let button = new Button(Text = "Nope", Left = 10,
-                                Top = 10, Width = 80, Enabled = true)
+  let form = new Form(Text = "Choosing the car")
+  let buttonAudi = new Button(Text = "Buy Audi", Left = 10,
+                              Top = 10, Width = 20, Enabled = (balance >= Audi.Price))
+  let buttonToyota = new Button(Text = "Buy Toyota", Left = 10,
+                                Top = 40, Width = 20, Enabled = (balance >= Toyota.Price))
+  let buttonHonda = new Button(Text = "Buy Honda", Left = 10,
+                               Top = 70, Width = 20, Enabled = (balance >= Honda.Price))
+  let buttonVolkswagen = new Button(Text = "Buy Volkswagen", Left = 10,
+                                    Top = 100, Width = 20, Enabled = (balance >= Volkswagen.Price))
+  let buttonFord = new Button(Text = "Buy Ford", Left = 10,
+                              Top = 130, Width = 80, Enabled = (balance >= Ford.Price))
+  let buttonMitsubishi = new Button(Text = "Bu Mitsubishi", Left = 10,
+                                    Top = 160, Width = 20, Enabled = (balance >= Mitsubishi.Price))
+  let buttonMersedes = new Button(Text = "Buy Mersedes", Left = 10,
+                                  Top = 190, Width = 20, Enabled = (balance >= Mersedes.Price))
 
-    form
+  form
 
 let LuckyForm = 
     
@@ -55,24 +69,32 @@ let mainForm =
 
     let form = new Form(Text = "Cars&Money")
 
+    let textBox = new TextBox(Text = balance.ToString(),
+                            Top = 100, Left = 150)
+
     let firstbutton = new Button(Text = "ChooseYourCar", Left = 10,
                                  Top = 10, Width = 100, Enabled = true)
-    let secondbutton = new Button(Text = "TestYourLuck", Left = 150,
-                                  Top = 10, Width = 100, Enabled = true)
-             
+    let secondbutton = new Button(Text = "TestYourLuck", Left = 10,
+                                  Top = 100, Width = 100, Enabled = true)
+    let thirdbutton = new Button(Text = "Ern your money", Left = 150,
+                                 Top = 10, Width = 100, Enabled = true)
                                                                                                 
-    let buttonPress1 _ _ = LuckyForm.ShowDialog |> ignore
-    let buttonPress2 _ _ = CarForm.ShowDialog |> ignore
+    let buttonPress1 _ _ = do Application.Run(CarForm)
+    let buttonPress2 _ _ = do Application.Run(LuckyForm)
+    let buttonPress3 _ _ = balance <- balance + CurrentCar.Ern
+                           MessageBox.Show(textBox.Text) |> ignore
 
     let eventHandler1 = new EventHandler(buttonPress1)
     let eventHandler2 = new EventHandler(buttonPress2)
+    let eventHandler3 = new EventHandler(buttonPress3)
 
     firstbutton.Click.AddHandler(eventHandler1)
     secondbutton.Click.AddHandler(eventHandler2) 
+    thirdbutton.Click.AddHandler(eventHandler3)
 
     let dc c = (c :> Control)
 
-    form.Controls.AddRange([| dc firstbutton; dc secondbutton;|])
+    form.Controls.AddRange([| dc firstbutton; dc secondbutton; dc thirdbutton|])
 
     form
 
